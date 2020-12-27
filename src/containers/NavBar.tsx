faeaac6cd947dc97a1d1b8e689bf763d25b5785c
DanDaMan23 <danlawrcons@gmail.com>
2020-12-27 00:35:33 -0600

    Navbar Finalized

--- src/containers/NavBar.tsx
+++ src/containers/NavBar.tsx
@@ -5,31 +5,34 @@ const NavBar: React.FC = (props) => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="#">
-          Social Media
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <Link to="/" className="nav-link">
-              Home
-            </Link>
-            <Link to="/posts" className="nav-link">
-              Posts
-            </Link>
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            Social Media
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+              <Link to="/posts" className="nav-link">
+                Posts
+              </Link>
+            </div>
           </div>
         </div>
       </nav>
+      <br/>
     </div>
   );
 };
